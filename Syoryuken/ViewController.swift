108827556582356c6e72cabc160f510525a5c09f
Nakaishi <esmt.n2006@gmail.com>
2023-08-29 14:45:45 +0900

    できた

--- Syoryuken/ViewController.swift
+++ Syoryuken/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //キャラクターを配置
+    @IBOutlet weak var aImage: UIImageView!
+    //空の配列を配置
+    var attackImageArray : [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //nillが返らなくなるまで配列に指定のイメージを追加
+        while let attackImage = UIImage(named: "attack\(attackImageArray.count+1)") {
+            attackImageArray.append(attackImage)
+        }
     }
-
-
+    
+    //アクションボタンの配置
+    @IBAction func startButton(_ sender: Any) {
+        // 配列を使ったアニメーションの配置
+        aImage.animationImages = attackImageArray
+        // イメージを切り替える間隔
+        aImage.animationDuration = 1.5
+        // アニメーションの繰り返し回数
+        aImage.animationRepeatCount = 1
+        // アニメーションを開始
+        aImage.startAnimating()
+    }
+    
+    
 }
-
